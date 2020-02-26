da2e7e0804ae012cd405d6396b6a4cdede7bad0c
Rachel <racmocon@gmail.com>
2020-02-26 11:14:09 -0500

    Fixed authentication issue

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
+  try {
   // This should be a token with access to your repository scoped in as a secret.
   // The YML workflow will need to set myToken with the GitHub Secret Token
   // myToken: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-  const myToken = core.getInput('GITHUB_TOKEN');
-  const label = core.getInput('tag_name');
+  const myToken = core.getInput(process.env.GITHUB_TOKEN);
+  const label = core.getInput('tag_name', { required: true });
 
   const octokit = new github.GitHub(myToken);
 
@@ -21,6 +22,10 @@ async function run() {
   });
 
   console.log(pullRequest);
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
 run();
