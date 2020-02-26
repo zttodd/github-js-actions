04fe0cc5de727a4896261a0bb626fda8e58b7127
Rachel <racmocon@gmail.com>
2020-02-26 14:35:49 -0500

    Added some console logs for testing

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const github = require('@actions/github');
 
 async function run() {
   try {
+    console.log('In the code.');
   // This should be a token with access to your repository scoped in as a secret.
   // The YML workflow will need to set myToken with the GitHub Secret Token
   // myToken: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
   const myToken = core.getInput(process.env.GITHUB_TOKEN);
+  console.log('Got mytoken');
   const tag = core.getInput('tag_name', { required: true });
+  console.log('tag: ' + tag);
   const label = tag.substr(9, tag.length-1);
+  console.log('label: ' + label);
 
   const octokit = new github.GitHub(myToken);
 
