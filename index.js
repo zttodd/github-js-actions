54a046144b7841b5c0735b39220e820662de6d4d
zttodd <zttodd@iu.edu>
2020-02-26 16:19:13 -0500

    Logging key entry

--- index.js
+++ index.js
@@ -20,19 +20,20 @@ async function run() {
       q: query
   });
 
-  let changelog;
+  // let changelog;
 
   // console.log(pullRequest.items);
 
   for (let [key, value] in Object.entries(pullRequest)) {
     if (key === 'items') {
+      console.log(key);
       key.forEach(function() {
         changelog += `- [${ value.title }](${ value.url })\n`;
       });
     }
   }
 
-  console.log(changelog);
+  // console.log(changelog);
 
   // core.setOutput('changelog', changelog);
 
