8ab7f1f51cf8d34f2dbcc8f6c9764f8868190c27
zttodd <zttodd@iu.edu>
2020-02-27 09:36:09 -0500

    Change case of github_token back to uppercase

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { GitHub } = require('@actions/github');
 
 async function run() {
   try {
-    const myToken = core.getInput('github_token');
+    const myToken = core.getInput('GITHUB_TOKEN');
 
     const tag = core.getInput('tag_name', { required: true });
     const label = tag.substr(10, tag.length-1);
