3ee21c41e7a088b8732f0c2f75ab940881f294d5
zttodd <zttodd@iu.edu>
2020-02-27 09:33:27 -0500

    Added name to build_changelog and added console.logs for troubleshooting

--- index.js
+++ index.js
@@ -3,30 +3,29 @@ const { GitHub } = require('@actions/github');
 
 async function run() {
   try {
-  // This should be a token with access to your repository scoped in as a secret.
-  // The YML workflow will need to set myToken with the GitHub Secret Token
-  // myToken: ${{ secrets.GITHUB_TOKEN }}
-  // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-  const myToken = core.getInput('GITHUB_TOKEN');
+    const myToken = core.getInput('github_token');
 
-  const tag = core.getInput('tag_name', { required: true });
-  const label = tag.substr(10, tag.length-1);
+    const tag = core.getInput('tag_name', { required: true });
+    const label = tag.substr(10, tag.length-1);
 
-  const query = `type:pr+label:${label}`;
+    const query = `type:pr+label:${label}`;
 
-  const octokit = new GitHub(myToken);
+    const octokit = new GitHub(myToken);
 
-  const { data: pullRequest } = await octokit.search.issuesAndPullRequests({
-      q: query
-  });
+    const { data: pullRequest } = await octokit.search.issuesAndPullRequests({
+        q: query
+    });
 
-  let changelog;
+    let changelog;
 
-  pullRequest.items.forEach(function(item) {
-    changelog += `- [${ item.title }](${ item.html_url })\n`;
-  })
+    pullRequest.items.forEach(function(item) {
+      console.log(item.title);
+      console.log(item.html_url);
 
-  core.setOutput('changelog', changelog);
+      changelog += `- [${ item.title }](${ item.html_url })\n`;
+    })
+
+    core.setOutput('changelog', changelog);
 
   } catch (error) {
     core.setFailed(error.message);
