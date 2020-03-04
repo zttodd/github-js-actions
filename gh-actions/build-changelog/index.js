849f9176e3a5963fd8a71798ed380be5745c3fdf
Rachel <racmocon@gmail.com>
2020-03-04 10:58:10 -0500

    Removed original files

--- gh-actions/build-changelog/index.js
+++ gh-actions/build-changelog/index.js
@@ -6,23 +6,24 @@ async function run() {
     const myToken = core.getInput('GITHUB_TOKEN');
 
     const tag = core.getInput('tag_name', { required: true });
-    const label = tag.substr(10, tag.length - 1);
+    const label = tag.substr(10, tag.length-1);
 
     const query = `type:pr+label:${label}`;
 
     const octokit = new GitHub(myToken);
 
     const { data: pullRequest } = await octokit.search.issuesAndPullRequests({
-      q: query
+        q: query
     });
 
     let changelog = '';
 
     pullRequest.items.forEach(function(item) {
-      changelog += `- [${item.title}](${item.html_url})\n`;
-    });
+      changelog += `- [${ item.title }](${ item.html_url })\n`;
+    })
 
     core.setOutput('changelog', changelog);
+
   } catch (error) {
     core.setFailed(error.message);
   }
