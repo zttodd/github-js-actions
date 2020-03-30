470276115eb8e50e3720bc49d4911b2ed610aca4
zttodd <zttodd@iu.edu>
2020-03-30 10:04:13 -0400

    Request to Github for PR information is working

--- changelog.js
+++ changelog.js
@@ -1,11 +1,37 @@
 // Include any node requirements (http, etc)
+const https = require('https');
 
 // Prompt for label/tag name
 
 // Make a connection to GitHub API and get JSON response
 // https://api.github.com/search/issues?q=repo:indiana-university/rivet-source+type:pr+label:v2.0.0-alpha.3
 
+const options = {
+  hostname: 'api.github.com',
+  port: 443,
+  path: `/search/issues?q=repo:indiana-university/rivet-source+type:pr+label:v2.0.0-alpha.3`,
+  method: 'GET',
+  headers: { 'User-Agent': 'rivet-source' }
+}
+
+let output;
+
+const req = https.request(options, res => {
+  output = res.statusCode;
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
+
 // From JSON response output, get pull request titles and html_urls for each PR
+console.log(output);
 
 // Construct titles and html_urls into markdown format of changelog
 
